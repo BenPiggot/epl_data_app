c417551cdb797ade8fc0520d78bbefc8c2d66932
wbp <wbpiggot@gmail.com>
2015-12-22 17:17:40 -0800

    Adding basic d3 bubble graph

--- app/assets/javascripts/graphics.js
+++ app/assets/javascripts/graphics.js
@@ -7,14 +7,74 @@ $(document).ready(function(){
        url: '/statistic/data',
        dataType: 'json',
        success: function (data) {
-         var div = document.createElement('div');
-         div.innerText = data
-         $('#canvas').append(div)
-         $(location).attr('href', '/statistic/data');
+         bubbleChart(data)
        }
     })
   })
 })
 
-d3.select('#graph')
-  .html("First!");
+
+var bubbleChart = function(payload) {
+
+  var json = { "players": {} };
+
+  payload.forEach( function(player) {
+    if (player.goals > 0) {
+      json.players[player.name] = player.goals
+    }
+  })
+  
+
+  var diameter = 900;
+
+  var svg = d3.select('#graph').append('svg')
+          .attr('width', diameter)
+          .attr('height', diameter);
+
+  var bubble = d3.layout.pack()
+        .size([diameter, diameter])
+        .value(function(d) {return d.size;})
+        .padding(3);
+
+  // generate data with calculated layout values
+  var nodes = bubble.nodes(processData(json))
+            .filter(function(d) { return !d.children; }); // filter out the outer bubble
+ 
+  var vis = svg.selectAll('g')
+          .data(nodes)
+          .enter().append('g')
+  
+  vis.append('circle')
+      .attr('transform', function(d) { 
+        return 'translate(' + (d.x) + ',' + d.y + ')'; 
+      })
+      .attr('r', function(d) { return d.r; })
+      .attr('class', function(d) { return d.className; });
+
+  vis.append('text')  
+      .attr('transform', function(d) { return 'translate(' + (d.x - 80)+ ',' + (d.y) + ')'; })
+      .style("font-family", "Helvetica Neue")
+      .style("color", "black")
+      .style('font-size', '1rem')
+      .text(function(d){
+        var goalText = d.size == 1 ? ' goal' : ' goals'
+        return d.name + ', ' + d.size + goalText
+      })
+  
+  function processData(data) {
+    var obj = data.players;
+
+    var newDataSet = [];
+
+    for(var prop in obj) {
+      newDataSet.push({name: prop, className: prop.toLowerCase(), size: obj[prop]});
+    }
+    return {children: newDataSet};
+  }
+  
+}
+
+
+
+
+
