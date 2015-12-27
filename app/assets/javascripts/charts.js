ad8bd5fd8019e28fde5626e6b43915b7e5aa4dbd
wbp <wbpiggot@gmail.com>
2015-12-26 16:18:50 -0800

    More d3 work

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
      url: '/graphic/data',
      dataType: 'json',
      success: function (data) {
-       makeChart(data)
+       makeCharts(data)
      }  
   })
 })
 
 
-function makeChart(data) {
+function makeCharts(data) {
 
   d3.select('.container').selectAll('div.names')
       .data(data)
@@ -30,4 +30,21 @@ function makeChart(data) {
       .attr('height', function(d) { return d.goals * 10})
       .attr('x', function(d, i ) { return i * 10 })
       .attr('y', function(d, i) { return 100 - (d.goals * 10) } )
+  
+  var plot =  d3.select('#scatterplot')
+    .selectAll('g')
+    .data(data)
+    .enter()
+    .append('g')
+    .attr('transform', function(d) {
+      return "translate(" + (d.minutes/10) + "," + (120 - (d.goals * 10)) + ")";
+    })
+
+  plot.append('circle')
+    .attr('r', function(d) {return d.goals * 2})
+    .attr('fill', '#ddd')
+
+  plot.append('text')
+    .text(function(d) { if (d.goals > 0) return d.name })
+    .attr('font-size', '8px')
 }
